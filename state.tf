f824e3449908827abcda0d0e9c4e81a086c655b0
chandana24 <gantachandana98@gmail.com>
2023-12-30 14:21:06 +0530

    Terraform

---
+++ state.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-dvp"
+    key    = "aws-parameters/terraform.tfstate"
+    region = "us-east-1"
+  }
+  }
+
+bucket = "tf-state-dvp"
+key    = "dev/terrafm.tfstate"
+region = "us-east-1"
